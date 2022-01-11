bea832793706a16d326c646965ff46fb3d0d5a54
Artem Eroshenko <eroshenkoam@qameta.io>
2022-01-11 21:46:41 +0300

    Initial commit

---
+++ src/test/java/qa/guru/allure/ParametersTest.java
@@ -0,0 +1,15 @@
+package qa.guru.allure;
+
+
+import io.qameta.allure.Allure;
+import org.junit.jupiter.api.Test;
+
+public class ParametersTest {
+
+    @Test
+    public void lambdaLabelsTest() {
+        Allure.parameter("Регион", "Москва");
+        Allure.parameter("Область", "Московская");
+    }
+
+}
